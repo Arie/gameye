9e90269be3da003192a68ed8fb731f7d0567ab2a
Arie <git@ariekanarie.nl>
2018-10-31 10:28:24 +0100

    Fix deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
 require "simplecov"
 require "coveralls"
 
-SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter])
 SimpleCov.start
 
 require "pry-nav"
